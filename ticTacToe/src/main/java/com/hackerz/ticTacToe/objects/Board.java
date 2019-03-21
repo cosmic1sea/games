fa558f9cee8618b2cea8e0502ae0b8768b9b2b4c
Jon Sierra <jonathan.sierra@ge.com>
2019-03-21 15:03:52 -0400

    win condition

--- ticTacToe/src/main/java/com/hackerz/ticTacToe/objects/Board.java
+++ ticTacToe/src/main/java/com/hackerz/ticTacToe/objects/Board.java
@@ -45,33 +45,40 @@ public class Board
             return true;
         }
         //Horizontal
-        if(theBoard.get("A1") == Mark.X && theBoard.get("A2") == Mark.X && theBoard.get("A3") == Mark.X)
+        if(theBoard.get("A1") == Mark.X && theBoard.get("A2") == Mark.X && theBoard.get("A3") == Mark.X
+                || theBoard.get("A1") == Mark.O && theBoard.get("A2") == Mark.O && theBoard.get("A3") == Mark.O)
         {
             return true;
         }
-        if(theBoard.get("B1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("B3") == Mark.X)
+        if(theBoard.get("B1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("B3") == Mark.X
+                || theBoard.get("B1") == Mark.O && theBoard.get("B2") == Mark.O && theBoard.get("B3") == Mark.O)
         {
             return true;
         }
-        if(theBoard.get("C1") == Mark.X && theBoard.get("C2") == Mark.X && theBoard.get("C3") == Mark.X)
+        if(theBoard.get("C1") == Mark.X && theBoard.get("C2") == Mark.X && theBoard.get("C3") == Mark.X
+                || theBoard.get("C1") == Mark.O && theBoard.get("C2") == Mark.O && theBoard.get("C3") == Mark.O)
         {
             return true;
         }
         //Vertical
-        if(theBoard.get("A1") == Mark.X && theBoard.get("B1") == Mark.X && theBoard.get("C1") == Mark.X)
+        if(theBoard.get("A1") == Mark.X && theBoard.get("B1") == Mark.X && theBoard.get("C1") == Mark.X
+                || theBoard.get("A1") == Mark.O && theBoard.get("B1") == Mark.O && theBoard.get("C1") == Mark.O)
         {
             return true;
         }
-        if(theBoard.get("A2") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("C2") == Mark.X)
+        if(theBoard.get("A2") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("C2") == Mark.X
+                || theBoard.get("A2") == Mark.O && theBoard.get("B2") == Mark.O && theBoard.get("C2") == Mark.O)
         {
             return true;
         }
         //Diagonal
-        if(theBoard.get("A1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("C3") == Mark.X)
+        if(theBoard.get("A1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("C3") == Mark.X
+                || theBoard.get("A1") == Mark.O && theBoard.get("B2") == Mark.O && theBoard.get("C3") == Mark.O)
         {
             return true;
         }
-        if(theBoard.get("C1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("A3") == Mark.X)
+        if(theBoard.get("C1") == Mark.X && theBoard.get("B2") == Mark.X && theBoard.get("A3") == Mark.X
+                || theBoard.get("C1") == Mark.O && theBoard.get("B2") == Mark.O && theBoard.get("A3") == Mark.O)
         {
             return true;
         }
