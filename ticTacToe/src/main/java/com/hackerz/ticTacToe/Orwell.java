5828eb36f0757b628fcd5166ba2b8de417616e72
Christopher Mosley <christopher.mosley1@ge.com>
2019-03-21 16:28:13 -0400

    sneaky throws

--- ticTacToe/src/main/java/com/hackerz/ticTacToe/Orwell.java
+++ ticTacToe/src/main/java/com/hackerz/ticTacToe/Orwell.java
@@ -1,27 +1,25 @@
+package com.hackerz.ticTacToe;
+
+import lombok.SneakyThrows;
+
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import com.hackerz.ticTacToe.objects.Board;
-
 public class Orwell
 {
-public boolean stateChange()
-{
-    public class Orwell
+    @SneakyThrows
+    public void writeToFile()
     {
-          
-        public void writeToFile()
+        Map<String, Character> theBoard = new HashMap<String, Character>();
+        Properties properties = new Properties();
+
+        for (Map.Entry<String, Character> entry : theBoard.entrySet())
         {
-            Map<String, Character> theBoard = new HashMap<String, Character>();
-            Properties properties = new Properties();
-
-            for (Map.Entry<String,Character> entry : theBoard.entrySet()) {
-                properties.put(entry.getKey(), entry.getValue());
-            }
-
-            properties.store(new FileOutputStream("file.name"), null);
+            properties.put(entry.getKey(), entry.getValue());
         }
-}
+
+        properties.store(new FileOutputStream("file.name"), null);
+    }
 }
