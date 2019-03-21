1ec598b6d9cfef391041b2d998a95d372d36e566
Christopher Mosley <christopher.mosley1@ge.com>
2019-03-21 15:41:58 -0400

    make Glados randomly pick a space

--- ticTacToe/src/main/java/com/hackerz/ticTacToe/players/Glados.java
+++ ticTacToe/src/main/java/com/hackerz/ticTacToe/players/Glados.java
@@ -1,23 +1,127 @@
 package com.hackerz.ticTacToe.players;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import com.hackerz.ticTacToe.objects.Mark;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * The all powerful machine learning robot.
+ */
 public class Glados implements Player
 {
+    /**
+     * Map of values which determine which spaces have the highest likelihood of winning.
+     */
     private final Map<String, Integer> weightedValues = new HashMap<>();
+
+    private final Character myMark = Mark.O;
+
+    private final Character enemyMark = Mark.X;
 
     @Override
     public Map<String, Character> makeMove(final Map<String, Character> currentBoard)
     {
         initWeightedValues();
-        evaluate(currentBoard);
+        final String newSpace = evaluate(currentBoard);
+        currentBoard.put(newSpace, myMark);
+        return currentBoard;
     }
 
-    private void evaluate(final Map<String, Character> currentBoard)
+    /**
+     * Currently randomly picks a move based on weight.
+     *
+     * @param currentBoard - the current board state
+     * @return new move
+     */
+    private String evaluate(final Map<String, Character> currentBoard)
     {
-//        if (currentBoard.get("B2").equals()
+        final Random random = new Random();
+
+        // Weight of 4
+        if (isSpaceEmpty("B2",currentBoard))
+        {
+            return "B2";
+        }
+
+        // Weight of 3
+        List<String> weightThreePossibilities = new ArrayList<>();
+
+        if (isSpaceEmpty("A1",currentBoard))
+        {
+            weightThreePossibilities.add("A1");
+        }
+        if (isSpaceEmpty("A3",currentBoard))
+        {
+            weightThreePossibilities.add("A3");
+        }
+        if (isSpaceEmpty("C1",currentBoard))
+        {
+            weightThreePossibilities.add("C1");
+        }
+        if (isSpaceEmpty("C3",currentBoard))
+        {
+            weightThreePossibilities.add("C3");
+        }
+
+        // Only one option
+        if (weightThreePossibilities.size() == 1)
+        {
+            return weightThreePossibilities.get(0);
+        }
+        else if (weightThreePossibilities.size() > 1)
+        {
+            return weightThreePossibilities.get(random.nextInt(weightThreePossibilities.size()));
+        }
+
+
+        // Weight of 2
+        List<String> weightTwoPossibilities = new ArrayList<>();
+
+        if (isSpaceEmpty("A2",currentBoard))
+        {
+            weightTwoPossibilities.add("A2");
+        }
+        if (isSpaceEmpty("B1",currentBoard))
+        {
+            weightTwoPossibilities.add("B1");
+        }
+        if (isSpaceEmpty("B3",currentBoard))
+        {
+            weightTwoPossibilities.add("B3");
+        }
+        if (isSpaceEmpty("C2",currentBoard))
+        {
+            weightTwoPossibilities.add("C2");
+        }
+
+        // Only one option
+        if (weightTwoPossibilities.size() == 1)
+        {
+            return weightTwoPossibilities.get(0);
+        }
+        else if (weightTwoPossibilities.size() > 1)
+        {
+            return weightTwoPossibilities.get(random.nextInt(weightTwoPossibilities.size()));
+        }
+        return null;
+    }
+
+    /**
+     * Is the given space available for our mark?
+     *
+     * @param coordinates - space in question
+     * @param currentBoard - current board state
+     * @return state of the coordinates
+     */
+    private boolean isSpaceEmpty(final String coordinates, final Map<String, Character> currentBoard)
+    {
+        // Is the enemy mark on a space or not empty? (Could be our mark)
+        return !(currentBoard.get(coordinates).equals(enemyMark) || !currentBoard.get(coordinates).equals(Mark.EMPTY));
     }
 
     private void initWeightedValues()
