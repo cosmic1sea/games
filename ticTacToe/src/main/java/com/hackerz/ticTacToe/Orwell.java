31654c2bdbd13d2aa6d30faf0a6a42305866f735
Jon Sierra <jonathan.sierra@ge.com>
2019-03-21 16:25:40 -0400

    rewrite

--- ticTacToe/src/main/java/com/hackerz/ticTacToe/Orwell.java
+++ ticTacToe/src/main/java/com/hackerz/ticTacToe/Orwell.java
@@ -1,11 +1,27 @@
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 import com.hackerz.ticTacToe.objects.Board;
 
 public class Orwell
 {
 public boolean stateChange()
 {
-    if()
-    
-    return false;
+    public class Orwell
+    {
+          
+        public void writeToFile()
+        {
+            Map<String, Character> theBoard = new HashMap<String, Character>();
+            Properties properties = new Properties();
+
+            for (Map.Entry<String,Character> entry : theBoard.entrySet()) {
+                properties.put(entry.getKey(), entry.getValue());
+            }
+
+            properties.store(new FileOutputStream("file.name"), null);
+        }
 }
 }
